878c53ba6faf2782168436c4a71b8f6e72de0af3
Diekrul <gianfranco.maiocchi@innovex.cl>
2026-04-17 17:10:46 -0400

    Fixed comments in Mapper and UserLocalDataSource

--- app/src/main/java/com/periodization/myapplication/data/local/UserLocalDataSource.kt
+++ app/src/main/java/com/periodization/myapplication/data/local/UserLocalDataSource.kt
@@ -1,4 +1,14 @@
 package com.periodization.myapplication.data.local
 
+import com.periodization.myapplication.data.model.UserEntity
+
+// Nos permite almacenar de manera local los datos del usuario.
+
 class UserLocalDataSource {
+    fun getCachedUser(): UserEntity {
+        return UserEntity(
+            nameStored = "Gianfranco Local",
+            ageStored = 29
+        )
+    }
 }
--- app/src/main/java/com/periodization/myapplication/data/mapper/UserMapper.kt
+++ app/src/main/java/com/periodization/myapplication/data/mapper/UserMapper.kt
@@ -4,8 +4,11 @@ import com.periodization.myapplication.data.model.UserDto
 import com.periodization.myapplication.data.model.UserEntity
 import com.periodization.myapplication.domain.model.User
 
-// El mapper traduce modelos de data al modelo de dominio.
-
+// El mapper convierte datos externos (API o base de datos) en modelos que usa la lógica de negocio.
+// El mapper no sabe de dónde vienen los datos. Solo sabe cómo transformarlos.”
+// Ejemplo:
+// API: { fullName: "Gianfranco", years: 30 }
+// Domain: User(name="Gianfranco", age=30)
 fun UserDto.toDomain(): User {
     return User(
         name = fullName,
